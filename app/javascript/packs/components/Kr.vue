b7b850d89fde0a3e71435a4fc2a4b095bb131ec4
Geoffrey <geoffrey.valero@otily.fr>
2021-04-30 00:04:03 +0200

    fix Kr edit mode + opti css

--- app/javascript/packs/components/Kr.vue
+++ app/javascript/packs/components/Kr.vue
@@ -1,7 +1,7 @@
 <template>
   
   <div class="row">
-    <div class="col-md-1" >
+    <div class="col-md-1">
       <a class="badge badge-elevo" href="#" @click.prevent="deleteKR"> - </a>
     </div>
     <div class="col-md-8" @click="InEditMode = true">
@@ -11,15 +11,24 @@
       {{copyKr.weight}}%
     </div>
     <div class="col-md-12">
-      <div class="row" v-if="InEditMode">
-        <div class="col-md-5"><input type="text" class="form-control" v-model="copyKr.title" ></div>
-        <div class="col-md-3"><input type="text" class="form-control" v-model="copyKr.weight" ></div>
-        <div class="col-md-1"> <div class="p2 badge badge-elevo"  @click="toggleEditMode()">-</div> </div>
-        <div class="col-md-1">  <div class="p2 badge badge-elevo"  @click="update">||</div></div>
+      <div class="row py-1" v-if="InEditMode">
+        
+        <div class="col-md-5 align-self-center"><input type="text" class="form-control" v-model="copyKr.title"></div>
+        
+        <div class="col-md-3 align-self-center"><input type="text" class="form-control" v-model="copyKr.weight"></div>
+        
+        <div class="col-md-1 text-center align-self-center">
+          <div style="padding: 5px 8px;" class="badge badge-elevo hover" @click="toggleEditMode()">-</div>
+        </div>
+        
+        <div class="col-md-1 text-center align-self-center">
+          <div style="padding: 5px 8px;" class="p2 badge badge-elevo hover" @click="update">||</div>
+        </div>
+      
       </div>
     </div>
   </div>
-  
+
 </template>
 
 <script>
@@ -27,38 +36,36 @@
         data: function () {
             return {
                 InEditMode: false,
-                copyKr:'',
+                copyKr: '',
             }
         },
-        props: ["index","kr"],
+        props: ["index", "kr"],
         methods: {
-            ToogleEditMode() {
-                console.log("edit mode");
+            toggleEditMode() {
                 this.InEditMode = !this.InEditMode;
-                console.log(this.isEditable);
             },
-            update(){
+            update() {
                 let vm = this;
-                axios.put("/api/v1/objectifs/"+vm.copyKr.objectif_id+"/krs/"+vm.copyKr.id, {
+                axios.put("/api/v1/objectifs/" + vm.copyKr.objectif_id + "/krs/" + vm.copyKr.id, {
                     _token: window.token,
-                    title:vm.copyKr.title,
-                    weight:vm.copyKr.weight,
+                    title: vm.copyKr.title,
+                    weight: vm.copyKr.weight,
                 })
                     .then(function (response) {
-                        let key_edit = "edit_kr_"+vm.copyKr.objectif_id;
-                        Event.$emit(key_edit, vm.index,response.data.data);
+                        let key_edit = "edit_kr_" + vm.copyKr.objectif_id;
+                        Event.$emit(key_edit, vm.index, response.data.data);
                         vm.InEditMode = false;
                     })
                     .catch(function (error) {
                     })
             },
-            deleteKR(){
+            deleteKR() {
                 let vm = this;
-                axios.delete('/api/v1/objectifs/'+vm.copyKr.objectif_id+'/krs/'+vm.copyKr.id, {
+                axios.delete('/api/v1/objectifs/' + vm.copyKr.objectif_id + '/krs/' + vm.copyKr.id, {
                     _token: window.token
                 })
                     .then(function (response) {
-                        let key_delete = "delete_kr_"+vm.copyKr.objectif_id;
+                        let key_delete = "delete_kr_" + vm.copyKr.objectif_id;
                         Event.$emit(key_delete, vm.index);
                     })
                     .catch(function (error) {
